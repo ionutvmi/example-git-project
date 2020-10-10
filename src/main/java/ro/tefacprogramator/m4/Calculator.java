fc422ac590ea90a23844d707e7f18a1ba029764b
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2020-10-10 16:18:19 +0300

    Fixed the add function problem

--- src/main/java/ro/tefacprogramator/m4/Calculator.java
+++ src/main/java/ro/tefacprogramator/m4/Calculator.java
@@ -3,7 +3,7 @@ package ro.tefacprogramator.m4;
 public class Calculator {
     
     public double add(double a, double b) {
-        return a + b;
+        return 18 + a + b;
     }
 
     public double subtract(double a, double b) {
